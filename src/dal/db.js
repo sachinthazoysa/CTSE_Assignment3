625de9667bdda5617372b730f90f9f01ee715807
A.D.S de Zoysa <it18230666@my.sliit.lk>
2022-05-13 20:10:45 +0530

    dockerizing done

--- src/dal/db.js
+++ src/dal/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const client = mongoose.connect(`${process.env.DATABASE_URL}${process.env.DATABASE_NAME}`, {
+const client = mongoose.connect(`${process.env.DATABASE_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true},
     
