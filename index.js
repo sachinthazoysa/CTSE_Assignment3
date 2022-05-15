7c786acca5c99b24e757abf89dd994adad725c35
A.D.S de Zoysa <it18230666@my.sliit.lk>
2022-05-16 00:36:26 +0530

    changed index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("User Management");
+    res.send("User Management v3 test");
 })
 
 app.use('/users', userAPI());
