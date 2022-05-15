bda4290e55915fb68a60f186dc709dcd1b64d4c4
A.D.S de Zoysa <it18230666@my.sliit.lk>
2022-05-16 01:29:51 +0530

    test4

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("User Management new repo v1");
+    res.send("User Management new repo v2");
 })
 
 app.use('/users', userAPI());
