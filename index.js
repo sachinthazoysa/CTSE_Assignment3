13d7be8bb195d7b69b3c0fb36830797d09e7a9e4
Dinod Sachintha de Zoysa <55564245+sachinthazoysa@users.noreply.github.com>
2022-05-17 00:05:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("User Management new repo v3");
+    res.send("User Management new repo v4");
 })
 
 app.use('/users', userAPI());
